665aa1eb1c76441180c5b6967a74ef47b6d9034a
zeuszcz <zeuszcz@mail.ru>
2025-03-25 01:06:23 +0700

    + rename

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-
+from src.models import CartAdmin,CartItemAdmin,BuildAdmin,ProductAdmin,ServiceAdmin,ComponentAdmin,ProductTypeAdmin
+from src.database import engine
 from src.routers.cart import router as router_cart
 from src.routers.product_component import router as router_component
 from src.routers.product import router as router_product
@@ -8,13 +9,14 @@ from src.routers.cart_item import router as router_cart_item
 from src.routers.service import router as router_service
 from src.routers.product_type import router as router_product_type
 from src.routers.build import router as router_build
-from dotenv import load_dotenv
+from sqladmin import Admin
 
 
 app = FastAPI(
-    title="calculator"
+    title="calculatora"
 )
 
+admin = Admin(app, engine)
 
 
 app.include_router(router_cart)
@@ -24,3 +26,24 @@ app.include_router(router_cart_item)
 app.include_router(router_service)
 app.include_router(router_product_type)
 app.include_router(router_build)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+admin.add_view(CartAdmin)
+admin.add_view(BuildAdmin)
+admin.add_view(CartItemAdmin)
+admin.add_view(ProductAdmin)
+admin.add_view(ProductTypeAdmin)
+admin.add_view(ServiceAdmin)
+admin.add_view(ComponentAdmin)
